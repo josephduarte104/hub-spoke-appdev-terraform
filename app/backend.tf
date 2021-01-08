d05d6c45851fbf450b6071b44c8dd4a9f6655d24
Joseph Duarte <joseph.duarte@utc.com>
2021-01-08 15:00:31 -0500

    updated backend

--- app/backend.tf
+++ app/backend.tf
@@ -1,3 +1,12 @@
 terraform {
   required_version = ">=0.12.6"
+  backend "azurerm" {
+    storage_account_name = "jdsharedservices104"
+    container_name       = "tfstate"
+    key                  = "prod-aks-app.terraform.tfstate"
+
+    # rather than defining this inline, the Access Key can also be sourced
+    # from an Environment Variable - more information is available below.
+    access_key = "RXvG4VBy36BIpRNTB1bW4akxhtj4fb6Cihi7CTERRaLS00Q2EsSSuDKpdZGCtVe1kzA/D6wlSwCiph2TpeBl4w=="
+  }
 }
--- hub/backend.tf
+++ hub/backend.tf
@@ -1,3 +1,12 @@
 terraform {
   required_version = ">=0.12.6"
+  backend "azurerm" {
+    subscription_id = "UTC Digital - DX - P5"
+    storage_account_name = "jdsharedservices104"
+    container_name       = "tfstate"
+    key                  = "prod-aks-hub.terraform.tfstate"
+    # rather than defining this inline, the Access Key can also be sourced
+    # from an Environment Variable - more information is available below.
+    access_key = "gsMqClBRKfsEpJuzCXYZwBupCm+Moc+JFgdtDLEumSD0XXPSw2oMadAlwJmMv6MjC1dI6Ps1L2CyX080/w9oQQ=="
+  }
 }
